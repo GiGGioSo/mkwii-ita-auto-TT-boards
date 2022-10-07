4c3880ecf9e9b38851d1e9d36e81f593102a5aff
FallBackITA27 <49619073+FallBackITA27@users.noreply.github.com>
2022-10-07 20:53:39 +0200

    Fixed two lines I accidentally swapped out.

--- src/gui/updater.py
+++ src/gui/updater.py
@@ -178,7 +178,7 @@ class Updater(QThread):
                             except: pass
                             with open("ghosts_flap/"+track_name+"/"+player_name.replace("*","")+".rkg","wb") as f:
                                 f.write(rkg)
-                self.display_msg.emit(f"{track_name} ({t_display}) took {time.time()-start_time_local} to update")
+                self.display_msg.emit(f"{track_name} {cat_name} ({t_display}) took {time.time()-start_time_local} to update")
                 total_time += time.time()-start_time
                 self.display_msg.emit(f"Total Time Elapsed: {total_time}\n")
                 log_out += f"Total Time Elapsed: {total_time}\n"
@@ -186,7 +186,7 @@ class Updater(QThread):
                 cat_n += 1
             gs.set_all_values(wks, full_gs)
             full_gs = gs.get_all_values(wks)
-            self.display_msg.emit(f"[SUCCESSFUL] Updated {track_name} {cat_name}, proceeding with the next track...")
+            self.display_msg.emit(f"[SUCCESSFUL] Updated {track_name}, proceeding with the next track...")
             with open("log_flap.txt","w") as f:
                 f.write(log_out)
         self.display_msg.emit("[FLAPS UPDATE FINISHED]")
@@ -317,7 +317,7 @@ class Updater(QThread):
                             except: pass
                             with open("ghosts_3lap/"+track_name+"/"+player_name.replace("*","")+".rkg","wb") as f:
                                 f.write(rkg_info)
-                self.display_msg.emit(f"{track_name} ({t_display}) took {time.time()-start_time_local} to update")
+                self.display_msg.emit(f"{track_name} {cat_name} ({t_display}) took {time.time()-start_time_local} to update")
                 total_time += time.time()-start_time
                 self.display_msg.emit(f"Total Time Elapsed: {total_time}")
                 log_out += f"Total Time Elapsed: {total_time}\n"
@@ -325,7 +325,7 @@ class Updater(QThread):
                 cat_n += 1
             gs.set_all_values(wks, full_gs)
             full_gs = gs.get_all_values(wks)
-            self.display_msg.emit(f"[SUCCESSFUL] Updated {track_name} {cat_name}, proceeding with the next track...")
+            self.display_msg.emit(f"[SUCCESSFUL] Updated {track_name}, proceeding with the next track...")
             with open("log_3lap.txt","w") as f:
                 f.write(log_out)
         gs.set_all_values(wks, full_gs)
